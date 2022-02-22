bf5ca8545ff66897c45ad2f97fe6ee392ac8f4c6
teun311 <hakiimulhoq@gmail.com>
2022-02-22 17:27:58 +0600

    upload

---
+++ first_project/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+	return 'hello world';
+});
+Route::get('/bitm',function (){
+    return'hello BITM';
+});
+Route::get('/basis',function (){
+    return view('demo');
+});
+Route::get('/arif',function (){
+    return view('arif');
+});
